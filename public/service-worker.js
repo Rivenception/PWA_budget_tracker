813fb4bdf77df780d5be4a79fed22f8759d0e9df
Rivenception <slicklootz@gmail.com>
2020-12-18 22:36:37 -0500

    added indexedDB.js, updated paths on manifest, added indexedDB to html

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,8 +6,7 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/index.js",
   "/manifest.webmanifest",
-  "/style.css",
-  "/assets/js/loadImages.js",
+  "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
